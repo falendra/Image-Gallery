c9372572dc54912b05b0d214f2786a162e92d5ea
Falendra Bandhe <sahilbandhe@gmail.com>
2019-02-20 15:30:58 +0530

    bootstrap added ,page button handled

---
+++ build/precache-manifest.dafeea4fe11ef33cc059081fefc4e007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edb8773f332b1f043118",
+    "url": "/static/css/main.10d163b7.chunk.css"
+  },
+  {
+    "revision": "edb8773f332b1f043118",
+    "url": "/static/js/main.edb8773f.chunk.js"
+  },
+  {
+    "revision": "d6a6ddd186ef71fc024c",
+    "url": "/static/js/1.d6a6ddd1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "2280d56aae46bbded00cb030ba660095",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,71 +1,79 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-
       pictures: [],
       value: '',
       page: 1,
+      totalPage: 1,
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.onSearchQueryChangeListener = this.onSearchQueryChangeListener.bind(this);
     this.pageHandler = this.pageHandler.bind(this);
+
+
   }
 
-  handleChange = (event) => {
+  onSearchQueryChangeListener = (event) => {
     let self = this;
-    console.log("handle change received value as "+event.target.value);
+    console.log("handle change received value as " + event.target.value);
     this.setState(
-      { value: event.target.value+"" },
-      () => { self.componentDidMount()}
+      { value: event.target.value + "" },
+      () => { self.componentDidMount() }
     );
 
-    // if (event.target.value === undefined || event.target.value === null) { return; }
-   
-    // console.log("inside handle changed: "+ this.state.value);
-   
+
   }
 
   pageHandler = () => {
+    console.log(this.state.totalPage);
     console.log(this.state.page);
-    this.setState({ page: this.state.page + 1 })
-    this.componentDidMount()
+    if (this.state.totalPage > this.state.page) {
+      this.setState({ page: this.state.page + 1 })
+      this.componentDidMount()
+    }
   }
 
   componentDidMount() {
-    if(this.state.value === null || this.state.value === undefined || this.state.value === ""){
+    if (this.state.value === null || this.state.value === undefined || this.state.value === "") {
       this.setState({ pictures: [] });
       return;
     }
-    console.log("inside mount: "+this.state.value);
+    console.log("inside mount: " + this.state.value);
     fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + process.env.REACT_APP_API_KEY + '&text=' + this.state.value + '&page=' + this.state.page + '&safe_search(1)&sort=relevance&format=json&nojsoncallback=1')
       .then(function (response) {
         return response.json();
       })
       .then(function (j) {
+
         /// j is response from api server in json format
-        console.log("data cereived as "+JSON.stringify(j));
-        
+
+
         if (j === undefined || j.photos === undefined || j.photos.photo === undefined) {
-          console.log("test1");
+          this.setState({ totalPage: 1, pictures: [] })
+
           return;
 
         }
-
+        this.setState({ totalPage: j.photos.total })
         let picArray = j.photos.photo.map((pic) => {
 
           var srcPath = 'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg';
+
+
           return (
 
-            <img alt="dogs" src={srcPath} className="IMAGE"></img>
-
+            <img alt="pics" src={srcPath} className="IMAGE" ></img>
           )
+
         })
         this.setState({ pictures: picArray });
+
       }.bind(this))
   }
 
@@ -75,13 +83,14 @@ class App extends Component {
     console.log("render function called.");
 
     return (
+
       <div className="App">
 
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Gallery</h1>
         </header>
-        <input type="text" className="Search-Bar" value={this.state.value} onChange={this.handleChange} placeholder="Search in gallery.." />
+        <input type="text" className="Search-Bar" value={this.state.value} onChange={this.onSearchQueryChangeListener} placeholder="Search in gallery.." />
 
 
         <p className="App-intro">
@@ -91,9 +100,9 @@ class App extends Component {
         </p>
 
 
-        <button onClick={this.pageHandler}>
-          change page
-      </button>
+        <Button variant="primary" onClick={this.pageHandler} size="lg" id="nextPageButton()" block>
+          Next Page >>
+        </Button>
 
       </div>
     );
